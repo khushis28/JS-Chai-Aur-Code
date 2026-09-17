e00517e81773c32d15a4a4042ee5d5ca0b00f9d5
Khushi Singh <khushiisingh100@gmail.com>
2026-09-17 17:44:33 +0000

    datatypes done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; //treat all JS code as newer version of JS
+
+alert(3
+    +3)  //code readability should be high
+
+console.log("Khushi");
+
+
+//primitive datatypes
+let name = "Ani"  //string
+let age = 23   //int
+let isLoggedIn = true   //boolean
+
+//number => 2 to power 53 or 52  -> range
+//bigint => used for larger number used
+//string => "", prefer double quotes
+//boolean => true/false
+
+/* null => standalone value and datatype as well, 
+it doesnt have any value but can be declared with variable, 
+i.e. let age = null, here null is just a value with nothing inside it, 
+for e.g. age = 0, here '0' is a value 
+but 'null' is itself not a value it's empty
+*/ 
+
+//undefined => it means it is not defined, only variable is declared
+//symbol => mostly useful for uniqueness
+
+
+//reference datatypes
+//object -> very important
+
+console.log(typeof "Khushi"); //gives 'string'
+console.log(typeof age); //gives 'number'
+console.log(typeof null); //gives type as 'object' -> interview imp Ques. **
+console.log(typeof undefined);  //gives type as 'undefined', same type & same value -> interview imp Ques. **
+
+
